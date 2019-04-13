da40ca7fed971b0e7512125c317b34399f325ff3
Thiago <thi_kun@hotmail.com>
2019-04-13 01:02:02 -0300

    Feita a funcionalidade de adicionar novos elementos pressionando Enter

--- js/components/form-zerfilos.js
+++ js/components/form-zerfilos.js
@@ -3,6 +3,18 @@
 const $formZerfilos = document.querySelector(".form-zerfilos");
 const $label = $formZerfilos.querySelector(".label");
 const $inputTxt = $formZerfilos.querySelector(".inputTxt");
+const $checkBoxes = $formZerfilos.querySelector(".checkBoxes");
+
+const newTag = value => {
+    const $tag = document.createElement("span");
+    const template = `
+            <input class="inputBox" id="${value}" type="checkbox" checked>
+            <label class="labelBox" for="${value}">${value}</label>`;
+    
+    $tag.innerHTML = template;
+
+    return $tag;
+}
 
 $inputTxt.addEventListener("focus", focusAndBlur);
 $inputTxt.addEventListener("blur", focusAndBlur);
@@ -11,3 +23,14 @@ function focusAndBlur() {
     $label.classList.toggle("-focus");
 }
 
+$formZerfilos.addEventListener('submit', event => event.preventDefault() ); 
+
+$inputTxt.addEventListener("keyup", event => {
+
+    if(event.key.toUpperCase() === "ENTER" || event.keyCode === 13){ // 13 sendo Enter em numérico
+        const value = $inputTxt.value;
+        $checkBoxes.appendChild(newTag(value));
+
+        $inputTxt.value = "";
+    }
+});
